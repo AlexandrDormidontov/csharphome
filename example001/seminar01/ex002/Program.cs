e3d90f9df150a98274dd305b450d807795f4c092
A. Dormidontov <adormidon@mail.ru>
2022-12-04 17:17:24 +0800

    random

---
+++ example001/seminar01/ex002/Program.cs
@@ -0,0 +1,7 @@
+﻿// метод рандомного выбора числа
+Console.WriteLine("start");
+
+int random = new Random().Next(0, 4); //выбираем их целых чисел: 0, 1, 2, 3
+Console.WriteLine (random); //выводится случайное целое число
+
+Console.WriteLine("end");
