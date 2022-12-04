d378eeebab9f7dd91f24bd9c6858219f884fe026
A. Dormidontov <adormidon@mail.ru>
2022-12-04 13:07:46 +0800

    решили первую задачу. найти квадрат числа.

---
+++ example001/seminar01/ex001/Program.cs
@@ -0,0 +1,7 @@
+﻿//Вывести квадрат числа
+Console.WriteLine("start"); //начало
+
+int number = 12; //определили целое число
+int result = number * number; //вычисление квадрата числа
+Console.WriteLine(result); //вывод результата 
+Console.WriteLine("end"); // окончание
