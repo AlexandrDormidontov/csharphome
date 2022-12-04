040502297263e504b91a5a82521ba8da6af0a9f1
A. Dormidontov <adormidon@mail.ru>
2022-12-04 23:27:09 +0800

    запутался с задачей

--- example001/homework001/ex02/Program.cs
+++ example001/homework001/ex02/Program.cs
@@ -4,3 +4,26 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("start");
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+if (b > max)
+{
+    if (b > c)
+    {
+        b = max;
+        Console.WriteLine("max");
+    }
+    else 
+    { 
+        c = max;
+        Console.WriteLine("max");
+        }
+ else
+ {
+    c > max;
+    max = c;
+ }       
+}
