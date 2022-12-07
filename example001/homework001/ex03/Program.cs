8a9de5a36502e873c95dd071fbe9ad9f7c9db262
A. Dormidontov <adormidon@mail.ru>
2022-12-07 22:40:35 +0800

    ex03

--- example001/homework001/ex03/Program.cs
+++ example001/homework001/ex03/Program.cs
@@ -6,3 +6,12 @@
 //7 -> нет
 
 Console.WriteLine("start");
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) //запись проверки числа на четность
+{
+    Console.WriteLine("число четное");
+}
+Console.WriteLine("end");
