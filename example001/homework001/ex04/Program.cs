1465cbb1c4228ff02277763d825cb424c3dfb497
A. Dormidontov <adormidon@mail.ru>
2022-12-08 23:15:34 +0800

    ex04

--- example001/homework001/ex04/Program.cs
+++ example001/homework001/ex04/Program.cs
@@ -4,18 +4,15 @@
 //8 -> 2, 4, 6, 8
 
 Console.WriteLine("start");
-
 Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("найти количество четных чисел от 1 до " + N);
 
-int i = 1;
-while (i <= N)
+for (int i = 1; i <= N; i++)
 {
     if (i % 2 == 0)
     {
-        Console.WriteLine(i + ", ");
-        i ++;
+       Console.WriteLine(i++);
     }
 }
 Console.WriteLine("end");
