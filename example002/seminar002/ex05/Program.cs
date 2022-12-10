6bab5b305c8ef24208ee4dcd3b217a88fcd58189
A. Dormidontov <adormidon@mail.ru>
2022-12-11 00:10:14 +0800

    ex05 end

---
+++ example002/seminar002/ex05/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[ ] array = { 1, 57, 13, 4, 5, 13, 57, 8 };
+
+int n = array.Length;
+int find = 57;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);// находит все индексы с заданным числом
+        break;// останавливается на первом найденном индексе заданного числа
+    }
+    //index = index + 1;
+    index ++;
+}
