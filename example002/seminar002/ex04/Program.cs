2e7624fd2023a0c0c78e97c4cf87510e3b5c1939
A. Dormidontov <adormidon@mail.ru>
2022-12-10 18:10:49 +0800

    ex04 start

---
+++ example002/seminar002/ex04/Program.cs
@@ -0,0 +1,13 @@
+﻿//определение массива
+int Max(int arg1, int arg2, int arg3) 
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}// индекс      0   1   2   3   4   5   6   7   8
+int [] array = {11, 12, 13, 14, 51, 61, 71, 18, 19} ;
+array [0] = 12; //записать знаение в массив
+
+Console.WriteLine(array[0]); //получить значение из массива
+
