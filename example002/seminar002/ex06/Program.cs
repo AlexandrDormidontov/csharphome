9e307a726baf516f7043699251348e12968c2e75
A. Dormidontov <adormidon@mail.ru>
2022-12-11 00:55:26 +0800

    ex06 metod voit

---
+++ example002/seminar002/ex06/Program.cs
@@ -0,0 +1,32 @@
+﻿//при применении метода void нельзя применять
+// возвращающее значение return.
+// Данный метод является не возвращающим
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); 
+        //index = index + 1
+        index ++;
+    } 
+
+
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position ++;
+        
+    }
+}
+int[] array = new int[10];// создаем массив по умолчанию 10 элементов
+
+FillArray(array);
+PrintArray(array);
