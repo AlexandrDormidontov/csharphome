5fa6a175b6ac40afe63f236761125c1187d3bbe8
A. Dormidontov <adormidon@mail.ru>
2022-12-12 21:45:08 +0800

    ex09

---
+++ homework002/ex09/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит третью
+//цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("start");
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while(a > 1000) a/= 10;
+Console.WriteLine(a%10);
+
+//while(a > 100);
+//Console.WriteLine(a%10);
+
+Console.WriteLine("end");
